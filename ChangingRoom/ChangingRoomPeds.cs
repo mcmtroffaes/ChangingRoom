b8e5701bf67b5d85e270c0f84d8b8405ec2f542d
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2015-10-11 10:16:23 +0100

    Add license notice to source files.

--- ChangingRoom/ChangingRoomPeds.cs
+++ ChangingRoom/ChangingRoomPeds.cs
@@ -1,3 +1,22 @@
+/*
+Changing Room - a GTA V Mod for changing models and outfits
+Copyright(C) 2015 Matthias C. M. Troffaes
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 // how to regenerate the bulk of this file:
 // wget https://raw.githubusercontent.com/crosire/scripthookvdotnet/dev/source/PedHashes.hpp
 // cat PedHashes.hpp | sed 's/\t//g' | sed 's/\(.*\) = .*/        PedHash.\1,/' | sed 'sX^// \(.*\)XEEENNNUUUMMM\1X' | sed '/EEENNNUUUMMM/ s/ //g' | sed 's/EEENNNUUUMMM\(.*\)/    public static PedHash[] models\1 = {/'
