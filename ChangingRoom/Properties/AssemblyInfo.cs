641d333cad66f295467427be8b6a4a0da3716d76
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2015-10-11 11:25:47 +0100

    Set description.

--- ChangingRoom/Properties/AssemblyInfo.cs
+++ ChangingRoom/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ChangingRoom")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("a GTA V Mod for changing models and outfits")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChangingRoom")]
